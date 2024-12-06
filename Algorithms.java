679448121f23831112a0385a2c570d3411cb2c84
jadaf49 <jadaf49@nycstudents.net>
2024-12-06 17:33:40 +0000

    Added two letter test code

--- Algorithms.java
+++ Algorithms.java
@@ -1,1 +1,25 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
+
+public class Algorithms
+{
+   private static File f;
+   private static Scanner s;
+   public static void main(String[] args) throws FileNotFoundException {
+       f = new File("words.txt");
+       int twoletter = twoletter()
+       System.out.println("Number of words with two letters: " + twoletter);
+
+public static int twoletter() throws FileNotFoundException{
+       s = new Scanner(f);
+       int twoletter = 0;
+       while (s.hasNext()) {
+            String currentword=s.nextString()
+            if (currentword.length()==2){
+                twoletter++;
+            }    
+       }
+       return twoletter;
+       
