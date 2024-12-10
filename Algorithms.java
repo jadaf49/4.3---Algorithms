ea3a3a7cea474c96f1e76b15bc5cd4a3a911669a
jadaf49 <jadaf49@nycstudents.net>
2024-12-10 22:21:29 +0000

    finished code

--- Algorithms.java
+++ Algorithms.java
@@ -7,19 +7,59 @@ public class Algorithms
 {
    private static File f;
    private static Scanner s;
+   private static Scanner b;
    public static void main(String[] args) throws FileNotFoundException {
        f = new File("words.txt");
-       int twoletter = twoletter()
-       System.out.println("Number of words with two letters: " + twoletter);
+       int twoletter = twoletter();
+       int longestword=longestword();
+       int palindromes=palindromes();
+       System.out.println("Number of two letter words: " + twoletter);
+       System.out.println("Number of words tied for the longest word in the file: " + longestword);
+       System.out.println("Number of palindromes: " + palindromes);
+       s.close();
+   }
 
-public static int twoletter() throws FileNotFoundException{
+
+   public static int twoletter() throws FileNotFoundException{
        s = new Scanner(f);
        int twoletter = 0;
        while (s.hasNext()) {
-            String currentword=s.nextString()
-            if (currentword.length()==2){
-                twoletter++;
-            }    
+           String currentword=s.nextLine();
+           if (currentword.length()==2) twoletter++;
        }
        return twoletter;
-       
+   }
+
+   public static int longestword() throws FileNotFoundException{
+    int longestword = 0;
+    int currentlength=0;
+    s = new Scanner(f);
+    while (s.hasNext()) {
+        String currentword=s.nextLine();
+        if (currentword.length()>currentlength) currentlength=currentword.length();
+    }
+    b = new Scanner(f);
+    while (b.hasNext()) {
+        String currentword=b.nextLine();
+        if (currentword.length()==currentlength) longestword++;
+    }
+    return longestword;
+}
+public static int palindromes() throws FileNotFoundException{
+    s = new Scanner(f);
+    int palindromes = 0;
+    while (s.hasNext()) {
+        String backwards = "";
+        String currentword=s.nextLine();
+        int wordlength=currentword.length();
+        while (wordlength>0){
+            String currentletter=currentword.substring(wordlength-1,wordlength);
+            backwards +=currentletter;
+            wordlength--;
+            if (backwards.equals(currentword)) palindromes++;
+        }
+        
+    }
+    return palindromes;
+}
+}
